338368c5b1459dc64352018a91e1e4dbc976054d
Suchanun <suchanun_sir@cmu.ac.th>
2025-07-03 13:24:50 +0700

    8.5
    done 8.5

--- main.js
+++ main.js
@@ -4,15 +4,27 @@ createApp({ setup() {
     const product = ref('Boots') 
     const brand = ref('SE 331')
     const description = ref('A pair of boots.') 
-    const image = ref('./assets/images/socks_green.jpg') 
-    const inStock = ref(true) 
+    //const image = ref('./assets/images/socks_green.jpg') 
+    //const inStock = ref(true) 
     const inventory = ref(11) 
     const details = ref([ '50% cotton', '30% wool', '20% polyester' ]) 
     const onSale = ref(true) 
     const variants = ref([ 
-        { id: 2234, color: 'green',image: './assets/images/socks_green.jpg' }, 
-        { id: 2235, color: 'blue' ,image: './assets/images/socks_blue.jpg'} 
-    ])     
+        //{ id: 2234, color: 'green',image: './assets/images/socks_green.jpg' }, 
+        //{ id: 2235, color: 'blue' ,image: './assets/images/socks_blue.jpg'} 
+        { id: 2234, color: 'green',image: './assets/images/socks_green.jpg', quantity: 50 }, 
+        { id: 2235, color: 'blue' ,image: './assets/images/socks_blue.jpg', quantity: 0} 
+    ])
+    const selectedVariant = ref(0)
+    function updateVariant(index) {  
+      selectedVariant.value = index;
+    }
+    const image = computed(() => {
+      return variants.value[selectedVariant.value].image
+    })
+    const inStock = computed(() => {
+      return variants.value[selectedVariant.value].quantity > 0
+    })   
     const cart = ref(0) 
     function addToCart() { cart.value += 1 } 
     const title = computed(() => {
@@ -28,6 +40,9 @@ createApp({ setup() {
     const produce2 = ref('CAMT')
     const description2 = ref('A Link to camt.')
     const url = ref('https://www.camt.cmu.ac.th')
+    const saleMessage = computed(() => {
+      return onSale.value ? `${brand.value} ${product.value} is on sale!` : ''
+    })
 
 return {
   //product,
@@ -36,6 +51,8 @@ return {
   description,
   image,
   inStock,
+  saleMessage,
+    updateVariant,
   inventory,
   details,
   onSale,
